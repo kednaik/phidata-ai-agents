acee6eb3ee79513cf428b8259eb5a8c46c862a0c
Kedar <kedar@Kedars-MacBook-Pro.local>
2025-01-17 19:23:21 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from utils import apply_styles
+from multi_agent import multi_ai_agent, as_stream
+
+st.title("ChatGPT-like clone")
+
+if st.button("💬 New Chat"):
+  st.session_state.messages = []
+  st.rerun()
+
+apply_styles()
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+
+  with st.chat_message("assistant"):
+    chunks = multi_ai_agent.run(prompt, stream=True)
+    response = st.write_stream(as_stream(chunks))
+  st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ multi_agent.py
@@ -0,0 +1,68 @@
+import os
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.python import PythonTools
+from phi.tools.wikipedia import WikipediaTools
+from phi.run.response import RunEvent, RunResponse
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+os.environ['GRQ_API_KEY'] = st.secrets.get('GROQ_API_KEY')
+
+
+web_search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for information",
+    model=Groq(id='llama-3.1-70b-versatile'),
+    tools=[DuckDuckGo()],
+    instructions=['Always include sources and references in your search results.'],
+    show_tool_calls=True,
+    markdown=True
+)
+
+financial_agent = Agent(
+    name="Financial Agent",
+    role="Get financial data",
+    model=Groq(id='llama-3.1-70b-versatile'),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True)],
+    instructions=['Use tables to display the data'],
+    show_tool_calls=True,
+    markdown=True
+)
+
+python_agent = Agent(tools=[PythonTools()], 
+                     show_tool_calls=True,
+                     role='To generate python code',
+                     model=Groq(id='llama-3.1-70b-versatile'),
+                    markdown=True)
+
+wikipedia_agent = Agent(tools=[WikipediaTools()], 
+                        show_tool_calls=True, 
+                        role='To get information from wikipedia',
+                        model=Groq(id='llama-3.1-70b-versatile'),
+                        markdown=True)
+
+
+multi_ai_agent = Agent(
+    team=[web_search_agent, financial_agent, python_agent, wikipedia_agent],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    instructions=['Always include sources and references in your search results.', 'Use tables to display the data'],
+    show_tool_calls=True,
+    markdown=True
+)
+
+# multi_ai_agent.print_response("Search information for NVDA stock", stream=True)
+
+def as_stream(response):
+  for chunk in response:
+    if isinstance(chunk, RunResponse) and isinstance(chunk.content, str):
+      if chunk.event == RunEvent.run_response:
+        yield chunk.content
+
+# multi_ai_agent.print_response("Write a python script for fibonacci series and display the result till the 10th number",stream=True)
+
+# multi_ai_agent.print_response('Summarize analyst recommendations and share the latest news for NVDA stock', stream=True)

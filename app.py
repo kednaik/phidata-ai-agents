2d1628418c927a2cec3915db45e0f7ea101c4a00
Kedar <kedar@Kedars-MacBook-Pro.local>
2025-01-18 23:35:28 -0500

    fix: formatting changes

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ from utils import apply_styles
 from multi_agent import multi_ai_agent, as_stream
 
 st.title("RAG Agentic AI Chatbot")
-st.markdown("This is a multi-agent system using GROQ model that can perform various tasks such as search web for info, aggregate finance/shares data, generate python code, search information from wikipedia, fetch metadata/caption from youtube video link.")
+st.markdown(
+    "This is a multi-agent system using GROQ model that can perform various tasks such as search web for info, aggregate finance/shares data, generate python code, search information from wikipedia, fetch metadata/caption from youtube video link."
+)
 
 if st.button("💬 New Chat"):
-  st.session_state.messages = []
-  st.rerun()
+    st.session_state.messages = []
+    st.rerun()
 
 apply_styles()
 
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-  st.session_state.messages.append({"role": "user", "content": prompt})
-  with st.chat_message("user"):
-    st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-  with st.chat_message("assistant"):
-    chunks = multi_ai_agent.run(prompt, stream=True)
-    response = st.write_stream(as_stream(chunks))
-  st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.chat_message("assistant"):
+        chunks = multi_ai_agent.run(prompt, stream=True)
+        response = st.write_stream(as_stream(chunks))
+    st.session_state.messages.append({"role": "assistant", "content": response})
--- multi_agent.py
+++ multi_agent.py
@@ -10,66 +10,88 @@ from phi.run.response import RunEvent, RunResponse
 import streamlit as st
 
 
-os.environ['GRQ_API_KEY'] = st.secrets.get('GROQ_API_KEY')
+os.environ["GRQ_API_KEY"] = st.secrets.get("GROQ_API_KEY")
 
 
 web_search_agent = Agent(
     name="Web Search Agent",
     role="Search the web for information",
-    model=Groq(id='llama-3.1-70b-versatile'),
+    model=Groq(id="llama-3.1-70b-versatile"),
     tools=[DuckDuckGo()],
-    instructions=['Always include sources and references in your search results.'],
+    instructions=["Always include sources and references in your search results."],
     show_tool_calls=True,
-    markdown=True
+    markdown=True,
 )
 
 financial_agent = Agent(
     name="Financial Agent",
     role="Get financial data",
-    model=Groq(id='llama-3.1-70b-versatile'),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True)],
-    instructions=['Use tables to display the data'],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    tools=[
+        YFinanceTools(
+            stock_price=True,
+            analyst_recommendations=True,
+            stock_fundamentals=True,
+            company_news=True,
+        )
+    ],
+    instructions=["Use tables to display the data"],
     show_tool_calls=True,
-    markdown=True
+    markdown=True,
 )
 
-python_agent = Agent(tools=[PythonTools()], 
-                     show_tool_calls=True,
-                     role='To generate python code',
-                     model=Groq(id='llama-3.1-70b-versatile'),
-                    markdown=True)
+python_agent = Agent(
+    tools=[PythonTools()],
+    show_tool_calls=True,
+    role="To generate python code",
+    model=Groq(id="llama-3.1-70b-versatile"),
+    markdown=True,
+)
 
-wikipedia_agent = Agent(tools=[WikipediaTools()], 
-                        show_tool_calls=True, 
-                        role='To get information from wikipedia',
-                        model=Groq(id='llama-3.1-70b-versatile'),
-                        markdown=True)
+wikipedia_agent = Agent(
+    tools=[WikipediaTools()],
+    show_tool_calls=True,
+    role="To get information from wikipedia",
+    model=Groq(id="llama-3.1-70b-versatile"),
+    markdown=True,
+)
 
 youtube_agent = Agent(
     tools=[YouTubeTools()],
     model=Groq(id="llama-3.1-70b-versatile"),
     role="Get captions and metadata of a YouTube video and answer questions",
     show_tool_calls=True,
-    description="You are a YouTube agent. Obtain the captions of a YouTube video and answer questions."
+    description="You are a YouTube agent. Obtain the captions of a YouTube video and answer questions.",
 )
 
 
 multi_ai_agent = Agent(
-    team=[web_search_agent, financial_agent, python_agent, wikipedia_agent, youtube_agent],
+    team=[
+        web_search_agent,
+        financial_agent,
+        python_agent,
+        wikipedia_agent,
+        youtube_agent,
+    ],
     model=Groq(id="llama-3.1-70b-versatile"),
-    instructions=['Always include sources and references in your search results.', 'Use tables to display the data'],
+    instructions=[
+        "Always include sources and references in your search results.",
+        "Use tables to display the data",
+    ],
     show_tool_calls=True,
     markdown=True,
-    add_chat_history_to_messages=True
+    add_chat_history_to_messages=True,
 )
 
 # multi_ai_agent.print_response("Search information for NVDA stock", stream=True)
 
+
 def as_stream(response):
-  for chunk in response:
-    if isinstance(chunk, RunResponse) and isinstance(chunk.content, str):
-      if chunk.event == RunEvent.run_response:
-        yield chunk.content
+    for chunk in response:
+        if isinstance(chunk, RunResponse) and isinstance(chunk.content, str):
+            if chunk.event == RunEvent.run_response:
+                yield chunk.content
+
 
 # multi_ai_agent.print_response("Write a python script for fibonacci series and display the result till the 10th number",stream=True)
 
